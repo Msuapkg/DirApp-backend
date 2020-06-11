476b4904139e8df7ff427d8dd4953789809171d9
Msuapkg <msuapkg@gmail.com>
2020-06-10 21:10:29 -0500

    Agregar ejemplos de embeber para bases de datos

--- models/Users.js
+++ models/Users.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable func-names */
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
@@ -29,13 +31,31 @@ const UsersSchema = mongoose.Schema({
     type: Boolean,
     default: true,
   },
+  /* // Ejemplo de manejar referenciada la cardinalidad de 1-1
+  // Reforzar un historial (Solo actualiza los cambios señalados, inserta el objeto)
+  address: {
+    street_name: {},
+    zip_code: {},
+    city: {},
+    country: {},
+  },
+  // Ejemplo de manejar referenciada la cardinalidad de 1-1
+  // Reforzar la integridad referencial (Actualiza todos los cambios)
+  address_ref: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Adresses',
+  }, */
+
 });
 
+// eslint-disable-next-line consistent-return
 UsersSchema.pre('save', function (next) {
   const user = this;
+
   // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) return next();
   // generate a salt
+  // eslint-disable-next-line consistent-return
   bcrypt.genSalt(SALT_WORK_FACTOR, (errHash, salt) => {
     if (errHash) return next(errHash);
     // hash the password along with our new salt
